ba0eceaebbff9b6bf906ee4a2a16010e94b8ea29
Maissa Mathlouthi <maissamth@gmail.com>
2025-04-21 07:40:28 +0100

    add Carousel component to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Header, Introduction } from '../components';
+import { Header, Introduction, Carousel } from '../components';
 
 const Home = () => (
   <div className="flex justify-between min-h-screen flex-col relative overflow-hidden" style={{ fontFamily: 'Poppins' }}>
     <Header />
     <Introduction />
+    <Carousel />
   </div>
 );
 
