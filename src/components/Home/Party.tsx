5812fe0f59de200b66346b23279938974b63edc1
Maissa Mathlouthi <maissamth@gmail.com>
2025-04-28 20:24:08 +0100

    add Party component to Home page with event booking details

---
+++ src/components/Home/Party.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'; // Import Image from Next.js
+
+export function Party () {
+  return (
+	<div className="flex justify-center items-center bg-center">
+	  <section className="w-full max-w-5xl mx-auto rounded-4xl py-14 lg:py-20 px-5 lg:px-0">
+		<div
+		  className="rounded-4xl px-5 py-5  my-1 mx-auto flex flex-col lg:flex-row gap-y-5 justify-center items-center"
+		  style={{
+			backgroundImage: "url('/BG/Party.png')", // Replace with your image path
+			backgroundSize: 'cover', // Ensure the image covers the entire section
+			backgroundPosition: 'center', // Center the image
+		  }}
+		>
+		
+		  {/* Text Content */}
+		  <div className="flex flex-col justify-center items-center text-left lg:text-left">
+		  <h1 className="text-gray-800 text-[30px] font-normal text-left leading-normal" style={{ fontFamily: 'Poppins' }}>
+		  Request an order for your private party or event? </h1>
+			<p className="text-gray-800 text-[20px] font-normal leading-normal" style={{ fontFamily: 'Poppins' }}>
+			We are open to ordering party events such as weddings, celebrations, or other events			</p>
+		  
+		  </div>
+		     {/* Store Button */}
+			 <div className="text-center p-4 my-4">
+        <a href="/store" className="btn btn-primary bg-black p-4  rounded-4xl text-white">Book Now</a>
+      </div>
+		</div>
+	  </section>
+	</div>
+  );
+}
+
+export default Party ;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Header, Introduction, Carousel } from '../components';
+import { Header, Introduction, Carousel, Party } from '../components';
 
 const Home = () => (
   <div className="flex justify-between min-h-screen flex-col relative overflow-hidden" style={{ fontFamily: 'Poppins' }}>
     <Header />
     <Introduction />
     <Carousel />
+    <Party />
   </div>
 );
 
