67bd50c779aae455428b034c21893b472b970559
Maissa Mathlouthi <maissamth@gmail.com>
2026-01-20 21:13:34 +0100

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,11 @@
 import '../styles/globals.css';
 import { Navbar, Footer, Chatbot } from '../components'; // Ensure these paths are correct
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+     
       <Navbar />
       <Component {...pageProps} />
       <Chatbot />
